f4784eedede1d0d94546e531954e444ca752175e
barisyild <fb_bcy@hotmail.com>
2023-07-04 01:16:32 +0300

    test

--- src/Main.hx
+++ src/Main.hx
@@ -1,5 +1,17 @@
+import utils.CompileTimeUtils;
 class Main {
     static public function main() {
         trace("Hello, world!");
+
+        var buildDate:Date = CompileTime.buildDate();
+        trace("Build date: " + buildDate);
+
+        var commitHash:String = CompileTimeUtils.buildGitCommitShaClean();
+        trace("Commit hash: " + commitHash);
+
+        Reflect.setField(js.Browser.window, "git", {
+            buildDate: buildDate,
+            commitHash: commitHash
+        });
     }
 }
