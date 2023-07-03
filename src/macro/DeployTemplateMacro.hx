2bb2224725d623a8bd7a554f91f585ade51fef70
barisyild <fb_bcy@hotmail.com>
2023-07-04 01:02:45 +0300

    test

---
+++ src/macro/DeployTemplateMacro.hx
@@ -0,0 +1,40 @@
+package macro;
+class DeployTemplateMacro {
+    public static function apply() {
+
+        try
+        {
+            // Create deploy folder
+            sys.FileSystem.createDirectory("deploy");
+
+            // Copy template folder to deploy folder
+            copyFolderContent("template", "deploy");
+        }
+        catch (e)
+        {
+            trace("Cannot copy template to deploy folder.");
+            throw e;
+        }
+    }
+
+    private static function copyFolderContent(source:String, destination:String):Void
+    {
+        // Read source folder contents
+        var sourceFolderContent:Array<String> = sys.FileSystem.readDirectory(source);
+
+        for(sourceContent in sourceFolderContent)
+        {
+            var sourcePath:String = source + "/" + sourceContent;
+            var destinationPath:String = destination + "/" + sourceContent;
+            if(sys.FileSystem.isDirectory(sourcePath))
+            {
+                sys.FileSystem.createDirectory(destinationPath);
+                copyFolderContent(sourcePath, destinationPath);
+            }
+            else
+            {
+                sys.io.File.copy(sourcePath, destinationPath);
+            }
+        }
+    }
+}
